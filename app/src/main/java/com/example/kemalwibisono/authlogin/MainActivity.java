313a85ffed1b06630b59166f6bc302cfaf309e76
Kemal Wibisono <wibisonokemal@gmail.com>
2019-08-12 20:17:29 +0700

    add some function

--- app/src/main/java/com/example/kemalwibisono/authlogin/MainActivity.java
+++ app/src/main/java/com/example/kemalwibisono/authlogin/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.kemalwibisono.authlogin;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
+    //clientid 501046837339-ueq79i5ufuugr795fdvkf84i3bn3giuo.apps.googleusercontent.com
+    //clientSecret        rXzuLFnKj4rkB6NRMBaTMKOD
 
+    private GoogleSignInClient gsc;
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        gsc = GoogleSignIn.getClient(this, gso);
+
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = gsc.getSignInIntent();
+                startActivityForResult(signInIntent,999);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK){
+            switch (requestCode){
+                case 999:
+                    Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                    try {
+                        GoogleSignInAccount account = task.getResult(ApiException.class);
+                        Log.d(TAG, "onActivityResult: Hellow "+account.getDisplayName());
+                    } catch (ApiException e) {
+                        e.printStackTrace();
+                        Log.d(TAG, "onActivityResult: failed sign in, code "+e.getStatusCode());
+                    }
+                    break;
+            }
+        }
     }
 }
